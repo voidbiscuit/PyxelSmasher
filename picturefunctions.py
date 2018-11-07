f1a194220838fca751a61476e43bdfa8b42caa01
Tim ^.^ <tim.underscore@gmail.com>
2018-11-07 16:30:32 +0000

    Update picturefunctions.py
    commented

--- picturefunctions.py
+++ picturefunctions.py
@@ -4,12 +4,28 @@ from PIL import Image, ImageFilter
 def modify(image=Image.NONE, choice=' ', params=[]):
     params = params if isinstance(params, list) else [params]
     if image == Image.NONE or choice == ' ': return image
+    
+    # Gaussian Blur
+    
     if choice == 'G':
         if len(params) < 1: return image
         if not str(params[0]).isdigit(): return image
         # print("Performing Gaussian " + str(params[0]))
         return image.filter(ImageFilter.GaussianBlur(int(params[0])))
+    
+    
+    # Image Enhance
+    
     if choice == 'N':
         # print("Enhancing Edges")
         return image.filter(ImageFilter.EDGE_ENHANCE)
+    
+    
+    # Fourier Transform
+    
+    
+    
+    
+    
+    # Return image anyway, in case none of the characters were recognised
     return image
