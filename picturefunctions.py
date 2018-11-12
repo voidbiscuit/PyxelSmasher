11ee3702944cd144eaed00b38c51cafa330a5b24
cx004763 <cx004763@student.reading.ac.uk>
2018-11-12 00:15:29 +0000

    magnitude spectrum looks nice

--- picturefunctions.py
+++ picturefunctions.py
@@ -36,10 +36,7 @@ def modify(image, choice=' ', params=0):
         # Magnitude of shift is exponential, apply log and multiply to get values ~ 0-150
         magnitude_spectrum = np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
         # Draw
-        plt.subplot(221), plt.imshow(image, cmap='gray')
-        plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-        plt.subplot(222), plt.imshow(magnitude_spectrum, cmap='gray')
-        plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+
         # X and Y (columns and rows) then divided by 2 as the rectangle is from the center
         cols, rows = image.size
         hcols, hrows = round(cols / 2), round(rows / 2)
@@ -54,11 +51,10 @@ def modify(image, choice=' ', params=0):
         else:
             xshift = round(hcols)
             yshift = round(hrows)
-
-
         # 2d array of 0s created, then replace a smaller rectangle with 1s as mask
         mask = np.zeros((rows, cols, 2), np.uint8)
-        mask[hrows - yshift:hrows + yshift, hcols - xshift:hcols + xshift] = 1
+        mask[hrows - yshift:hrows + yshift, hcols - xshift:hcols + xshift] = [1, 1]
+        magnitude_spectrum[:] *= (mask[:, :, 0] + 1) / 2
         # Some weird code to filter some magnitude spectrum stuff.
         # Multiply shift with mask for masked shift, and inverse the shift
         fshift = dft_shift * mask
@@ -68,11 +64,17 @@ def modify(image, choice=' ', params=0):
         img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
         # Convert picture back to BMP
         formatted = (img_back * 255 / np.max(img_back)).astype('uint8')
-        image = Image.fromarray(formatted)
+        img_back = Image.fromarray(formatted)
+
+        plt.subplot(221), plt.imshow(image, cmap='gray')
+        plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(222), plt.imshow(magnitude_spectrum, cmap='gray')
+        plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
         plt.subplot(223), plt.imshow(mask[:, :, 0], cmap='gray')
         plt.title('Mask'), plt.xticks([]), plt.yticks([])
-        plt.subplot(224), plt.imshow(image, cmap='gray')
+        plt.subplot(224), plt.imshow(img_back, cmap='gray')
         plt.title("Result [" + str(xshift) + " " + str(yshift) + "]"), plt.xticks([]), plt.yticks([])
         plt.show()
+        image = img_back
 
     return image  # If invalid option, return
